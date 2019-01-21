cdff3066bffaa7150fbf0c8e57e2f7e11e7eb336
NurislamS <snurislam97@mail.ru>
2019-01-21 18:45:25 +0300

    master

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 		Event event  = new Event();
 		event.setEvent("im a new event");
 		event.setEvent("im a pirat, hahahah");
+		event.setEvent("no, here im a main branch");
         System.out.println("Hello, " + event.getEvent() + "main branch");
     }
 }
