20f9504bba158a47e3576dcdd8f0159d58b9c424
NurislamS <snurislam97@mail.ru>
2019-01-21 18:38:29 +0300

    new event

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 		Event event  = new Event();
 		event.setEvent("im a new event");
-        System.out.println("Hello, " + event.getEvent());
+        System.out.println("Hello, " + event.getEvent() + "main branch");
     }
 }
