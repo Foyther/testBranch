363e25fc2559cef2e28bf4a03e06c6a0103bd446
NurislamS <snurislam97@mail.ru>
2019-01-22 16:28:30 +0300

    new branch

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		event.setEvent("new game");
 		event.setEvent("new game2");
 		event.setEvent("uzhe old game");
+		event.setEvent("uzhe old game 2");
         System.out.println("Hello, " + event.getEvent() + "main branch");
     }
 }
