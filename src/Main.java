a313da6a2c0a9bda8330f2a547d1bd92b5e92ab1
NurislamS <snurislam97@mail.ru>
2019-01-22 16:36:27 +0300

    master

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 		Event event  = new Event();
-		event.setEvent("new game");
-		event.setEvent("new game2");
-		event.setEvent("uzhe old game");
-		event.setEvent("uzhe old game 2");
+		event.setEvent("master");
         System.out.println("Hello, " + event.getEvent() + "main branch");
     }
 }
