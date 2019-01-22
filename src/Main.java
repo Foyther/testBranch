c285f886f7a3899123e08d220d6e348014ddcc92
NurislamS <snurislam97@mail.ru>
2019-01-22 16:35:20 +0300

    new branch

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 		Event event  = new Event();
-		event.setEvent("master");
+		event.setEvent("new game");
         System.out.println("Hello, " + event.getEvent() + "main branch");
     }
 }
